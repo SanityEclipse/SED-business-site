280a6a3e0bd70e8f833d9a8c48d176fe2dfcb0ef
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2017-12-17 19:21:01 -0700

    Added routerlink anchor tags to navigate through all views- all working correctly.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  imports:      [ CommonModule ],
+  imports:      [ CommonModule, RouterModule ],
   declarations: [ HeaderComponent, NavbarComponent ],
   exports:      [ HeaderComponent, NavbarComponent ],
   providers:    []
