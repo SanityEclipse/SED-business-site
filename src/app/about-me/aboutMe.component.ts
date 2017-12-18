24b4dcea96f923d189a9f90d9e56ba144ea49350
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2017-12-17 18:25:52 -0700

    created lazy views folder and initial component.

---
+++ src/app/about-me/aboutMe.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me',
+  template:
+  `
+  <h1>This is the 'About Me' Component</h1>
+  `,
+  styles:[]
+})
+
+
+export class AboutMeComponent {}
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  template:
+  `
+  <h1>This is the 'Portfolio' Component</h1>
+  `,
+  styles:[]
+})
+
+
+export class PortfolioComponent {}
