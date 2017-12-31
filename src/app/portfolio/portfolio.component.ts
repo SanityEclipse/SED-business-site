9e95d9431c0a040546b8d2bac3f7b16bc59d4158
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2017-12-31 14:06:50 -0700

    Added basic scaffolding and images assets.

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
-  template:
-  `
-  <h1>This is the 'Portfolio' Component</h1>
-  `,
+  templateUrl: './portfolio.component.html',
   styles:[]
 })
 
