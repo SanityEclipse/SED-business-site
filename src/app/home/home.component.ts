0773e6f6e5564491030991031b2f26b8e168ebe0
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2018-01-07 17:36:48 -0700

    Added animations folder and modules.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
+import { fadeInAnimation } from '../animations/index';
+
 @Component({
+  moduleId: module.id.toString(),
   selector: 'home-screen',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
