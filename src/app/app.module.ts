85b595d90eab65140bb396ffb4365311c1b1c8d3
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2018-01-07 17:24:37 -0700

    Updated all dependencies, added ng2 dependencies to app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule }    from '@angular/platform-browser';
-import { NgModule }         from '@angular/core';
+import { NgModule }                from '@angular/core';
+import { BrowserModule }           from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent }     from './app.component';
 
@@ -10,6 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CoreModule,
     AppRoutingModule
   ],
@@ -19,4 +21,4 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
