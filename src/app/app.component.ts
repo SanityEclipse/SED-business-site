e3f2da27db8bf500b20a62121ffca5cf500b7285
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2018-01-10 14:44:18 -0700

    Refactored animation code into its own file. Removed from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,11 @@
 import { Component } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  query,
-} from '@angular/animations';
+import { routerTransition } from './_animations/router-transition.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('routerAnimation', [
-      transition('* <=> *', [
-        // INITIAL STATE
-        query(':enter',
-          style({
-            opacity: 0,
-            position: 'fixed',
-            width:'100%',
-          }), {optional:true}
-        ),
-        // PAGE LEAVING
-        query(':leave',
-          animate('500ms ease',
-            style({
-              opacity: 0,
-              position: 'fixed',
-              width:'100%',
-            })
-          ),
-          {optional:true}
-        ),
-        // PAGE ENTERING
-        query(':enter',
-          animate('500ms ease',
-            style({
-              opacity: 1,
-            })
-          ),
-          {optional:true}
-        ),
-      ])
-    ])
-  ]
+  animations: [routerTransition]
 })
 
 export class AppComponent {
